d83943146d3e8cbe56a1d3fbb278caada6560eb4
mithlesh12345 <97218339+mithlesh12345@users.noreply.github.com>
2022-01-06 13:09:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var ball
+function setup() {
+  createCanvas(400,400);
+ball=createSprite(200,200,20,20)
+
+
+
+
+
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+  ball.position.x+=5
+  }
+  
+
+
+
+
+drawSprites()
+}
+
+
+
+
